dc4629d4d0a8f9a720ee420b0da6327f273d5f4f
mahmudmhb1 <mahmud.mhb1@gmail.com>
2021-12-07 23:56:51 +0600

    responsive

--- src/Portfolio/Menus/Menu/Contact/Contact.js
+++ src/Portfolio/Menus/Menu/Contact/Contact.js
@@ -25,22 +25,22 @@ const Contact = () => {
             <h2 className="fh-bold text-white ">CONTACT <span className="text-primary">ME</span></h2>
 
                 <div className="row">
-                    <div className="col-md-6">
+                    <div className=" col-12 col-md-6">
                         <div>
-                            <img src={image} alt="" />
+                            <img class="img-fluid"  src={image} alt="" />
                         </div>
                     </div>
-                    <div className="col-md-6">
+                    <div className="col-12 col-md-6">
 
-                    <div className=" mt-5 pt-5">
+                    <div className=" mt-5  pt-5">
                         <form action="" ref={form} onSubmit={sendEmail}>
-                                <input type="text" class=" mb-3 py-1 contact" name="name" id="exampleFormControlInput1" placeholder="Your name" required/><br />
-                                <input type="email" class="mb-3 py-1 contact" name="email" id="exampleFormControlInput1" placeholder="Your E-mail" required/> <br />
+                                <input type="text" class=" mb-3 rounded-3 w-75 py-1 " name="name" id="exampleFormControlInput1" placeholder="Your name" required/><br />
+                                <input type="email" class="mb-3 py-1 w-75 rounded-3" name="email" id="exampleFormControlInput1" placeholder="Your E-mail" required/> <br />
                                 
-                                <textarea class="contact py-1 mb-3" required id="exampleFormControlTextarea1" name="message" placeholder="Your Message" rows="3"></textarea>
+                                <textarea class=" py-1 rounded-3 w-75 mb-3" required id="exampleFormControlTextarea1" name="message" placeholder="Your Message" rows="3"></textarea>
                                 <br />
                             
-                                <input type="submit" className="btn btn-primary contact" value="SUBMIT"/>
+                                <input type="submit" className="btn btn-primary w-75 rounded-3 " value="SUBMIT"/>
                         </form>
             
             </div>
